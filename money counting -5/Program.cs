3bc82da8c0b4a9ab011b1a9bef82b9d055c38b23
lilblock1 <58580672+lilblock1@users.noreply.github.com>
2021-03-18 15:54:28 -0500

    Add files via upload

---
+++ money counting -5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace quiz_ai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("games only 5$ cash");
+
+            int cash = Convert.ToInt32(Console.ReadLine());
+
+            if (cash < 5)
+             {
+              Console.WriteLine("Thats not all the money.");
+            }
+            else if (cash == 5)
+             {
+                Console.WriteLine("here are your games");
+            }
+            else
+            {
+            int change = cash - 5;
+            Console.WriteLine("here are your games and " + change + " dallers in change left over");
+            }
+
+            // wait before closing
+            Console.ReadKey();
+        }
+    }
+}
